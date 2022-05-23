dd204b589af94c7f39144a7b4de3c66176564298
Viktorija Gončarova <v.goncarova.work@gmail.com>
2022-05-23 23:07:43 +0300

    firebase hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,9 +7,14 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    //outDir: "build",
+    prerender: {
+      default: true,
+    },
+    trailingSlash: 'always',
+  },
 };
 
 export default config;
